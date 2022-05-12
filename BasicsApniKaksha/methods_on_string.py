51be1cbb09ff62f268779a127e2de4a7dfa02a04
Raghav Jajoo <61447812+Raghavjajooc123@users.noreply.github.com>
2022-05-13 02:39:09 +0530

    Create methods_on_string.py

---
+++ BasicsApniKaksha/methods_on_string.py
@@ -0,0 +1,12 @@
+# methods on strings
+
+name = "raghav"
+
+print(name.upper())
+print(name.find('g'))
+print(name.find('ha'))
+print(name.replace("raghav", "jajoo"))
+
+print('r' in name)
+print("rag" in name)
+print('z' in name)
